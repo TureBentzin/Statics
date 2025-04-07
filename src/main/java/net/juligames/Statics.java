06a73951074b0d69f760356eb1ad874cda697780
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-04-07 19:14:47 +0200

    placeholders work

--- src/main/java/net/juligames/Statics.java
+++ src/main/java/net/juligames/Statics.java
@@ -1,5 +1,8 @@
 package net.juligames;
 
+import io.github.miniplaceholders.api.Expansion;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.Tag;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Statics extends JavaPlugin {
@@ -7,7 +10,16 @@ public final class Statics extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Plugin is starting up!");
 
+        Expansion.Builder builder = Expansion.builder("statics");
+
+        builder.globalPlaceholder("demo", (argumentQueue, context) -> {
+            return Tag.inserting(Component.text("Hello, World!"));
+        });
+
+        Expansion expansion = builder.build();
+        expansion.register();
     }
 
     @Override
